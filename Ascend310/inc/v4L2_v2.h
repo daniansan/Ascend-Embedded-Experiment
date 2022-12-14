2def8e99ae3c193089e586e0b67c320881811c81
lailiang2333 <92515840+lailiang2333@users.noreply.github.com>
2022-12-14 20:08:31 +0800

    Create v4L2_v2.h

---
+++ Ascend310/inc/v4L2_v2.h
@@ -0,0 +1,31 @@
+#ifndef _V4L2_V2_H_
+#define _V4L2_V2_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+ 
+#include <assert.h>
+#include <getopt.h>           
+#include <fcntl.h>            
+#include <unistd.h>
+#include <errno.h>
+#include <malloc.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+ 
+ 
+ 
+ 
+#include <asm/types.h>        
+#include <linux/videodev2.h>
+
+
+
+void v4l2_test();
+
+
+#endif // !_V4L2_V2_H_
